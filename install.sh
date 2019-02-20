178af373e0596b119e55f2ac5c267214d6785a7c
Vitaly <vit.butoma@gmail.com>
2019-02-20 23:22:19 +0300

    init commit'

---
+++ install.sh
@@ -0,0 +1,5 @@
+npm install -g composer-cli@0.20
+npm install -g composer-rest-server@0.20
+npm install -g generator-hyperledger-composer@0.20
+npm install -g yo
+npm install -g composer-playground@0.20
