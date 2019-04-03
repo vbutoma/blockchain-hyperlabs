fd82172635db7ce8a090fb0e6e00b595c090c018
Vitaly <vit.butoma@gmail.com>
2019-04-03 19:57:49 +0300

    temp

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 npm install -g composer-cli@0.20
 npm install -g composer-rest-server@0.20
 npm install -g generator-hyperledger-composer@0.20
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd dev-servers
+
+./startFabric.sh
+
+sleep 1
+
+./createPeerAdminCard.sh
